c1833fb77e5c2124f7049027cf77bfc21c98611a
djonnybravo <djonnybravo29@gmail.com>
2023-05-16 16:09:11 +0300

    fix: AddItemForm.tsx

--- src/components/AddItemForm.tsx
+++ src/components/AddItemForm.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
-const AddItemForm = () => {
+type AddItemFormPropsType = {
+    todolistID: string
+    addTask: (title: string, todolistID: string) => void
+}
+
+const AddItemForm = (props: AddItemFormPropsType) => {
+
+    const [newTaskTitle, setNewTaskTitle] = useState(' ')
+    const [error, setError] = useState(false)
+
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTaskTitle(e.currentTarget.value)
+        setError(false)
+    }
+    const onKeyPressInputHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.charCode == 13) {
+            props.addTask(newTaskTitle, props.todolistID);
+            setNewTaskTitle('')
+            setError(false)
+        }
+    }
+    const addTask = () => {
+        if (newTaskTitle.trim() !== "") {
+            props.addTask(newTaskTitle, props.todolistID);
+            setNewTaskTitle('')
+
+        } else {
+            setError(true)
+        }
+    }
+
     return (
         <div>
-            
+            <input
+                value={newTaskTitle}
+                onChange={onChangeInputHandler}
+                onKeyPress={onKeyPressInputHandler}
+                className={error ? "error" : ""}
+            />
+            <button onClick={addTask}>+</button>
+            {error && <div className="error-message">Field is required and cant be empty!</div>}
+
         </div>
     );
 };
