e32b9059dfdaeb3a4b03485b9cc4064f877cd1de
djonnybravo <djonnybravo29@gmail.com>
2023-05-16 20:20:58 +0300

    add MUI to project

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -2,6 +2,8 @@ import React, {ChangeEvent} from 'react';
 import {FilterValuesType} from "../App";
 import AddItemForm from "./AddItemForm";
 import EditableSpan from './EditableSpan';
+import {Button, ButtonGroup, Checkbox, IconButton} from "@mui/material";
+import {Delete, DeleteOutline} from "@mui/icons-material";
 
 
 type PropsType = {
@@ -30,14 +32,18 @@ const Todolist = (props: PropsType) => {
     const onCompletedFilterClick = () => props.changeFilter('Completed', props.todolistID)
     const removeTodolist = () => props.removeTodolist(props.todolistID)
     const addTask = (title: string) => props.addTask(title, props.todolistID)
-    const onChangeTodolistTitle = (title:string) =>  props.changeTodolistTitle(title, props.todolistID)
+    const onChangeTodolistTitle = (title: string) => props.changeTodolistTitle(title, props.todolistID)
 
 
     return (
         <div>
             <h3>
                 <EditableSpan title={props.title} onChange={onChangeTodolistTitle}/>
-                <button onClick={removeTodolist}>x</button>
+                <IconButton onClick={removeTodolist}>
+                    <DeleteOutline/>
+                </IconButton>
+
+                {/*<button onClick={removeTodolist}>x</button>*/}
             </h3>
 
             <AddItemForm addItem={addTask}/>
@@ -50,28 +56,34 @@ const Todolist = (props: PropsType) => {
                             const onChangeTaskTitle = (title: string) => props.changeTaskTitle(task.id, title, props.todolistID)
 
                             return <li key={task.id} className={task.isDone ? "is-done" : ""}>
-                                <input type="checkbox" checked={task.isDone} onChange={onChangeStatus}/>
+                                <Checkbox checked={task.isDone} onChange={onChangeStatus}
+                                          color={task.isDone ? "success" : "info"}/>
                                 <EditableSpan title={task.title} onChange={onChangeTaskTitle}/>
-
-                                <button onClick={removeTask}>x
-                                </button>
+                                <IconButton onClick={removeTask}>
+                                    <DeleteOutline fontSize="small" color={task.isDone ? "success" : "primary"}/>
+                                </IconButton>
                             </li>
                         }
                     )}
             </ul>
             <div>
-                <button
-                    className={props.filter === "All" ? "active-filter" : ""}
-                    onClick={onAllFilterClick}>All
-                </button>
-                <button
-                    className={props.filter === "Active" ? "active-filter" : ""}
-                    onClick={onActiveFilterClick}>Active
-                </button>
-                <button
-                    className={props.filter === "Completed" ? "active-filter" : ""}
-                    onClick={onCompletedFilterClick}>Completed
-                </button>
+                <Button
+                    onClick={onAllFilterClick}
+                    variant={props.filter === "All" ? "contained" : "outlined"}
+                    color={props.filter === "All" ? "secondary" : "primary"}
+
+                >All</Button>
+                <Button onClick={onActiveFilterClick}
+                        variant={props.filter === "Active" ? "contained" : "outlined"}
+                        color={props.filter === "Active" ? "info" : "primary"}
+                >Active
+                </Button>
+                <Button onClick={onCompletedFilterClick}
+                        variant={props.filter === "Completed" ? "contained" : "outlined"}
+                        color={props.filter === "Completed" ? "success" : "primary"}
+                >Completed
+                </Button>
+
             </div>
         </div>
     );
