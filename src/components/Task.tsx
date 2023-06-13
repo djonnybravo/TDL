8fb3a7e71b19e368ee3a0120d708167e592e1797
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-06-13 23:07:13 +0300

    feat: create RFC for tasks

---
+++ src/components/Task.tsx
@@ -0,0 +1,33 @@
+import React, {ChangeEvent, memo, useCallback} from 'react';
+import {Checkbox, IconButton} from "@mui/material";
+import EditableSpan from "./EditableSpan";
+import {DeleteOutline} from "@mui/icons-material";
+import {TaskType} from "./Todolist";
+
+type TaskPropsType = {
+    removeTask: (id: string, todolistID: string) => void
+    changeTaskStatus: (taskID: string, isDone: boolean, todolistID: string) => void
+    changeTaskTitle: (id: string, title: string, todolistID: string) => void
+    task: TaskType
+    todolistID: string
+}
+
+const Task = memo((props: TaskPropsType) => {
+
+    const removeTask = () => props.removeTask(props.task.id, props.todolistID)
+    const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => props.changeTaskStatus(props.task.id, e.currentTarget.checked, props.todolistID)
+    const onChangeTaskTitle = useCallback((title: string) => props.changeTaskTitle(props.task.id, title, props.todolistID), [props.task.id, props.todolistID])
+
+    return (
+        <li key={props.task.id} className={props.task.isDone ? "is-done" : ""}>
+        <Checkbox checked={props.task.isDone} onChange={onChangeStatus}
+                  color={props.task.isDone ? "success" : "info"}/>
+        <EditableSpan title={props.task.title} onChange={onChangeTaskTitle}/>
+        <IconButton onClick={removeTask}>
+            <DeleteOutline fontSize="small" color={props.task.isDone ? "success" : "primary"}/>
+        </IconButton>
+    </li>
+    )
+});
+
+export default Task;
