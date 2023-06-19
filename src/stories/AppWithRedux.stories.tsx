fc14784520c7bb0fe96b4ab257c0b75c5dbe67ee
djonnybravo <djonnybravo29@gmail.com>
2023-06-19 18:20:34 +0300

    feat: create  AppWithRedux.stories

---
+++ src/stories/AppWithRedux.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import React from "react";
+import AppWithRedux from "../AppWithRedux";
+import {ReduxStoreProviderDecorator} from "../state/ReduxStoreProviderDecorator";
+
+
+const meta: Meta<typeof AppWithRedux> = {
+  title: 'TODOLISTS/AppWithRedux',
+  component: AppWithRedux,
+  tags: ['autodocs'],
+  decorators: [ReduxStoreProviderDecorator]
+
+};
+
+export default meta;
+type Story = StoryObj<typeof AppWithRedux>;
+
+export const AppWithReduxStory: Story = {
+  // render: () =>  <Provider store={store}> <AppWithRedux/> </Provider>
+};
+
+
