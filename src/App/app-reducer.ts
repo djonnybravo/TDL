431f0953d02f35d04fe4d101e11f6f9f49815a81
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-09 23:14:31 +0300

    feat: add appreducer

---
+++ src/App/app-reducer.ts
@@ -0,0 +1,34 @@
+
+
+
+
+
+export const AppReducer = (state: initialStateType = initialState, action: ActionsTypes): initialStateType => {
+
+    switch (action.type) {
+
+        case "APP/SET-STATUS":
+            return {...state, status: action.status}
+        case "APP/SET-ERROR":
+            return {...state, error: action.error}
+
+        default: return state
+    }
+
+
+}
+
+
+const initialState: initialStateType = {
+    status: "idle",
+    error: null
+}
+
+type initialStateType = {
+
+    status: 'idle' | 'loading' | 'success' | 'failed'
+    error: string | null
+}
+
+type ActionsTypes = any
+}
