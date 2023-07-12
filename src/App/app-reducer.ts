5a88ff57fda5ae8e0d26375508825ffeee491715
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-07-12 19:55:17 +0300

    fix: refactor

--- src/App/app-reducer.ts
+++ src/App/app-reducer.ts
@@ -28,8 +28,8 @@ type initialStateType = {
     error: string | null
 }
 export type RequestStatusType = 'idle' | 'loading' | 'success' | 'failed'
-export type AppActionsTypes = SetErrorType |SetStatusType
-export type SetStatusType =  ReturnType<typeof setStatusAC>
-export type SetErrorType =  ReturnType<typeof setErrorAC>
-export const setStatusAC = (status: RequestStatusType) => ({type: "APP/SET-STATUS", status} as const)
-export const setErrorAC = (error: string | null) => ({type: "APP/SET-ERROR", error} as const)
+export type AppActionsTypes = SetAppErrorActionType |SetAppStatusActionType
+export type SetAppStatusActionType =  ReturnType<typeof setAppStatusAC>
+export type SetAppErrorActionType =  ReturnType<typeof setAppErrorAC>
+export const setAppStatusAC = (status: RequestStatusType) => ({type: "APP/SET-STATUS", status} as const)
+export const setAppErrorAC = (error: string | null) => ({type: "APP/SET-ERROR", error} as const)
