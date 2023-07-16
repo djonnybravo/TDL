c71ef76f668c5c21f9ab75b6c194a425aaa3babd
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-07-16 21:42:07 +0300

    feat: add authApi.ts

---
+++ src/api/auth/authApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    withCredentials: true,
+    headers: {
+        "API-KEY": "d49d0d6f-9d1b-4840-b7a2-147b39bd1e13"
+    }
+})
+
+
+export const authAPI = {
+    isLogged(id: number, email: string, login: string) {
+        return instance.get('auth/me')
+    },
+    login(email: string, password: string) {
+        return instance.post('auth/login', {email, password})
+    }
+}
