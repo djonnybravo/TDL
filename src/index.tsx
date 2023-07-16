22d700ccd499d14040f15f91f9321902d4c6e30f
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-07-16 17:08:13 +0300

    feat: add routing

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,19 @@ import './index.css';
 import App from "./App/App";
 import {Provider} from "react-redux";
 import {store} from "./App/store";
+import {BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
 
-        <Provider store={store}>
-            <App/>
-        </Provider>
 
 
 );
