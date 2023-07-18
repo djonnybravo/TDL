1707eef8c5d3cc6ef98f4c697172766745edf130
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-18 19:46:31 +0300

    feat: add logout req

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -41,7 +41,7 @@ function App() {
             <AppHeader/>
             <Routes>
                 <Route  path={'/'} element={<TodolistList/>}/>
-                <Route  path={'/login'} element={<Login/>}/>
+                <Route  path={'/logIn'} element={<Login/>}/>
                 <Route  path={"/404"} element={<h1>404: PAGE NOT FOUND</h1>}/>
                 <Route  path={"*"} element={<Navigate to={"/404"}/>}/>
             </Routes>
