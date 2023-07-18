5d2e7e7bb07b61882296e8260dd0a3b1a514c73f
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-18 19:28:43 +0300

    feat: add logout req

--- src/features/AppBar/AppHeader.tsx
+++ src/features/AppBar/AppHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import {AppBar, Box, Button, IconButton, LinearProgress, Toolbar, Typography} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import {useSelector} from "react-redux";
-import {AppRootStateType} from "../../App/store";
+import {AppRootStateType, useAppDispatch} from "../../App/store";
 import {RequestStatusType} from "../../App/app-reducer";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -11,6 +11,7 @@ import IconButton from "@mui/material/IconButton";
 import LinearProgress from "@mui/material/LinearProgress";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import {logoutTC} from "../Login/auth-reducer";
 
 
 
@@ -18,8 +19,13 @@ import Typography from "@mui/material/Typography";
 
 
 const AppHeader = () => {
-
+    const isLoggedIn = useSelector<AppRootStateType>(state => state.auth.isLoggedIn)
     const requestStatus = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
+    const dispatch = useAppDispatch()
+
+    const logout = () => {
+        dispatch(logoutTC())
+    }
 
 
     return (
@@ -38,7 +44,7 @@ const AppHeader = () => {
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                         News
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    { !isLoggedIn && <Button color="inherit" onClick={logout}>Log out</Button>}
                 </Toolbar>
                 {
                     requestStatus === "loading" && <LinearProgress/>
