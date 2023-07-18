c4bb26c54852005ecc2e7d20ec45e37f448b72b2
Evgeniy Solovev <djonnybravo29@gmail.com>
2023-07-18 19:14:58 +0300

    feat: add init req

--- src/App/app-reducer.ts
+++ src/App/app-reducer.ts
@@ -11,6 +11,8 @@ export const AppReducer = (state: initialStateType = initialState, action: AppAc
             return {...state, status: action.status}
         case "APP/SET-ERROR":
             return {...state, error: action.error}
+        case "APP/SET-INITIALIZED" :
+            return {...state, isInitialized: action.isInitialized}
 
         default: return state
     }
@@ -21,15 +23,18 @@ export const AppReducer = (state: initialStateType = initialState, action: AppAc
 
 const initialState: initialStateType = {
     status: "loading",
-    error: null
+    error: null,
+    isInitialized: false
 }
 type initialStateType = {
     status: RequestStatusType
     error: string | null
+    isInitialized: boolean
 }
 export type RequestStatusType = 'idle' | 'loading' | 'success' | 'failed'
-export type AppActionsTypes = SetAppErrorActionType |SetAppStatusActionType
+export type AppActionsTypes = SetAppErrorActionType |SetAppStatusActionType | ReturnType<typeof setIsInitializedAC>
 export type SetAppStatusActionType =  ReturnType<typeof setAppStatusAC>
 export type SetAppErrorActionType =  ReturnType<typeof setAppErrorAC>
 export const setAppStatusAC = (status: RequestStatusType) => ({type: "APP/SET-STATUS", status} as const)
 export const setAppErrorAC = (error: string | null) => ({type: "APP/SET-ERROR", error} as const)
+export const setIsInitializedAC = (isInitialized: boolean) => ({type: "APP/SET-INITIALIZED", isInitialized} as const)
